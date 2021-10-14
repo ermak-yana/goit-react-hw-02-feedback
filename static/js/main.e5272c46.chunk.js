24943d5b20dcd0821ced570d269148c76dddbc91
ermak-yana <lidl.peperts.stor@gmail.com>
2021-10-14 19:48:43 +0300

    Updates

---
+++ static/js/main.e5272c46.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(t,e,c){"use strict";c.r(e);var n=c(1),r=c.n(n),i=c(3),o=c.n(i),a=(c(8),c(9),c(0));var s=function(){return Object(a.jsx)("div",{})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(t,e,c){},9:function(t,e,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.e5272c46.chunk.js.map
