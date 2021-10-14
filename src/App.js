9d1f71946b983f3aafde25d2946e8941febe965c
ermak-yana <lidl.peperts.stor@gmail.com>
2021-10-14 23:28:27 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
+import { Component } from 'react';
+import FeedbackList from './components/Feedback/FeedbackList';
+import Section from './components/Section';
+import Statistics from './components/Statistics';
 
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
 
-export default App;
+handleCount = e => {
+  const name = e.currentTarget.name;
+  this.setState(prevState => ({
+    [name]: prevState[name] + 1,
+  }));
+};
+countTotalFeedback = () => {
+  const { good, neutral, bad } = this.state;
+  return good + neutral + bad;
+};
+countPositiveFeedbackPercentage = () => {
+  const { good } = this.state;
+  return parseInt((good / this.countTotalFeedback()) * 100);
+};
+
+  render() {
+    const { good, neutral, bad } = this.state;
+  return (
+    <>
+    <Section title="Please leave feedback">
+      <FeedbackList
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={this.handleCount}
+      />
+    </Section>
+    <Section title="Statistics">
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={this.countTotalFeedback()}
+        positivePercentage={this.countPositiveFeedbackPercentage()}
+      />
+    </Section>
+  </>
+  );
+    }
+}
+export default App
+
